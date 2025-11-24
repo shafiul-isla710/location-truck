dd5d721a12cf23b9e8a46345242da49a3e6ae148
shafiul <shafiul7sms@gmail.com>
2025-11-24 16:46:36 +0600

    JWT Authentication setup ,User login successfully and create token

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
@@ -11,6 +12,7 @@ use App\Http\Requests\RegisterRequest;
 class AuthController extends Controller
 {
     
+    // User Registration
     public function register(RegisterRequest $request)
     {
         try{
@@ -27,6 +29,31 @@ class AuthController extends Controller
         catch(\Exception $e){
             Log::error('Registration Error: '.$e->getMessage());
             return $this->errorResponse('Registration failed',500);
+        }
+    }
+
+    //User login
+    public function login(LoginRequest $request)
+    {
+        try{
+            $request->validated();
+
+            $credentials = $request->only('email', 'password');
+            
+            if (!$token = auth()->attempt($credentials)) {
+                return response()->json(['error' => 'Invalid Credentials'], 401);
+            }
+
+            return response()->json([
+                'token' => $token,
+                'token_type' => 'bearer',
+                // 'expires_in' => auth()->factory('api')->getTTL() * 60
+            ]);
+        }
+        
+        catch(\Exception $e){
+            Log::error('Login Error: '.$e->getMessage());
+            return $this->errorResponse('Login failed',500);
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
